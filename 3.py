d9007450ea25bc005c65b5222214474e7c459060
Ruthik5689 <sunkararuthik@gmail.com>
2026-02-03 09:16:39 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,26 @@
+def minCost(str1, str2):
+    n = len(str1)
+    diff = []
+    for i in range(n):
+        if str1[i] != str2[i]:
+            diff.append(i)
+    if not diff:
+        return 0
+    res = 0
+    i = 0
+    while i < len(diff) - 1:
+        if diff[i+1] - diff[i] == 1:
+            res += 1
+            i += 2
+        else:
+            res += 2
+            i += 1
+    if i < len(diff):
+        res += 1
+    return res
+
+
+str1 = input("Enter string 1 :")
+str2 = input("Enter string 2 :")
+
+print(minCost(str1, str2))
